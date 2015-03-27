59dd07d8c1699499c781a180ab9377dd8509348c
shrishail sh <shrishail.sh92@gmail.com>
2015-03-27 23:33:19 +0530

    added new sub menu

---
+++ header.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="images/icon.png">
+
+    <title>Fist | Header</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- Custom styles for this template -->
+    <link href="carousel.css" rel="stylesheet">
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+
+    <div class="navbar-wrapper">
+
+        <nav class="navbar navbar-inverse">
+
+            <ul class="nav navbar-nav adjust">
+              <li>
+                <a href="index.php">
+                  <img src="images/Helping_Hands_cropped.jpg" class="logo" height="55px" width="55px" alt="logo"/>
+                </a>
+              </li>
+              <li class="navbar-brand"><h1><FONT FACE="algerian">Future India Social Trust</FONT></h1></li>
+              <li>
+              </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                  <div class="col-lg-12">
+                    <div class="input-group">
+                      <input type="search" class="form-control searchbox" placeholder="Search here...">
+                      <span class="input-group-btn">
+                        <button class="btn btn-default searchbutton" type="button"><i class="glyphicon glyphicon-search"></i></button>
+                      </span>
+                    </div><!-- /input-group -->
+                  </div><!-- /.col-lg-6 -->
+                </li>
+            </ul>
+
+
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            </div>
+
+            <div class="navbar-collapse collapse" id="navbar">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="home.php"><i class="glyphicon glyphicon-home"></i>&nbsp;Home</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Hemophilia <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="history_hemophilia.php">History</a></li>
+                    <li><a href="statistics_hemophilia.php">Statistics</a></li>
+                    <li><a href="about_hemophilia.php">About Himophilia</a></li>
+                    <li class="divider"></li>
+                    <li class="dropdown-header">Know More</li>
+                    <li><a href="#">Ongoing Researches</a></li>
+                    <li><a href="#">Employment Opportunities</a></li>
+                  </ul>
+                </li>
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Patient Education <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="guides_hemophilia.php">Guides / Manuals</a></li>
+                    <li><a href="glossary_hemophilia.php">Glossary</a></li>
+                  </ul>
+                </li>
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Health Care <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Medicines</a></li>
+                    <li><a href="treatment_center_search.php">Treatment Centers</a></li>
+                  </ul>
+                </li>
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Projects <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Ongoing</a></li>
+                    <li><a href="#">Completed</a></li>
+                  </ul>
+                </li>
+
+                <li><a href="#about">About us</a></li>
+                <li><a href="#contact"><i class="glyphicon glyphicon-phone-alt"></i>&nbsp;Contact us</a></li>
+
+              </ul>
+
+              <ul class="nav navbar-nav navbar-right">
+                <div class="">
+                    <div class="pull-right hidden-xs">
+                      <a href="#" class="" data-toggle="dropdown"><h5><i class="glyphicon glyphicon-user"></i>&nbsp;<i class="glyphicon glyphicon-collapse-down"></i></h5></a>
+                      <ul class="dropdown-menu">
+                          <li><a href="#"><i class="glyphicon glyphicon-log-in"></i> Signin</a></li>
+                          <li><a href="#"><i class="glyphicon glyphicon-share-alt"></i> Signup</a></li>
+                      </ul>
+                    </div>
+                  </div>
+              </ul>
+
+            </div>
+
+        </nav>
+      <div id="headline"><span>The best way to find yourself is to lose yourself in the service of others.</span></div>
+    </div>
+
+    <a href="#" class="scrollToTop" title="Scroll to top"><i class="glyphicon glyphicon-chevron-up"></i></a>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
