70ecb14f8537a34ec7637c42775d7579bf9edb63
shrishail92 <shrishail.sh92@gmail.com>
2015-03-29 03:46:34 +0530

    finished statistics page

---
+++ statistics_hemophilia.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="icon.png">
+
+    <title>Fist | Statistics of hemophilia</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="offcanvas.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <?php include "header.php" ?>
+    <div class="container marketing" id="container">
+      
+      <div class="row row-offcanvas row-offcanvas-right">
+        
+        <div class="col-xs-12 col-sm-9">
+          <p class="pull-right visible-xs">
+            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
+          </p>
+          <div class="jumbotron">
+            <h2 class="featurette-heading">Statistics of <span class="text-muted">Hemophilia</span></h2>
+            <p class="lead text-justify">
+            Hemophilia is a rare hereditary bleeding disorder. This disease occurs due to deficiency of clotting factors 
+            resulting in the blood taking prolonged time to clot. Hemophilia occurs in about 1 of every 5,000 male births. 
+            Hemophilia A is about four times as common as hemophilia B, and about half of those affected have the severe form. 
+            Hemophilia affects people from all racial and ethnic groups. 
+            </p>
+          </div>
+
+          <div class="row">
+            <div class="col-xs-6 col-lg-12">
+              <h2>Hemophilia in <span class="text-muted">the world</span></h2><br/>
+            </div>
+            <div class="col-lg-12">
+              <img class="img-responsive img-thumbnail" src="images/statworld.png" alt="Generic placeholder image">
+            </div>
+          </div>
+          <p class="text-justify">   
+          <br/><span class="para">
+          Global distribution of total reported cases of bleeding disorders (a) and haemophilia A (c) in five countries 
+          reporting the highest number of patients. (b) and (d) show that nearly 5 and 9% of global patients with bleeding 
+          disorders and haemophilia A are from India. 
+          </span><br/>
+          </p>
+
+          <div class="row">
+            <div class="col-xs-6 col-lg-12">
+              <h2>Hemophilia in <span class="text-muted">India</span></h2><br/>
+            </div>
+            <div class="col-lg-12">
+              <img class="img-responsive img-thumbnail" src="images/statindia.png" alt="Generic placeholder image">
+            </div>
+          </div>
+          <p class="text-justify">   
+          <br/><span class="para">
+          In India, the incidence of hemophilia is nearly 1 in 5000 males. It has been postulated that in our country 
+          1300 children each year are born with hemophilia. And it is observed (calculated at 0.9 per 1,00,000) and estimated 
+          (calculated at 4 per 1,00,000 population) prevalence of haemophilia A for states and union territories of India.
+          </span><br/>
+          </p>
+          
+          <div class="row">
+            <div class="col-xs-6 col-lg-12">
+              <h2>Comparision among <span class="text-muted">affected countries</span></h2><br/>
+            </div>
+            <div class="col-lg-12">
+              <img class="img-responsive img-thumbnail" src="images/stat.png" alt="Generic placeholder image">
+            </div>
+          </div>
+          <p class="text-justify">   
+          <br/><span class="para">
+          (a) Haemophilia A trends in four high burden countries. Numbers in parenthesis show the number of patients reported in 
+          the last five years by each country. (b) Per capita clotting factor VIII consumption (Y-axis) and number of haemophilia 
+          patients reported in 2011 for the four high burden countries.
+          </span><br/>
+          </p>
+
+        </div><!--/.col-xs-12.col-sm-9-->
+
+        <?php include "sidebar_menu.php"; ?>
+        <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
+          <div class="list-group">
+            <?php include("fb_like_box.php") ?>
+          </div>
+        </div>
+
+      </div><!--/row-->
+
+    </div><!--/.container-->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+    <script src="offcanvas.js"></script>
+    <?php include 'footer.php'; ?>
+  </body>
+</html>
