cf3838253dea60edcf548361121aa731e2b546b2
shrishail92 <shrishail.sh92@gmail.com>
2015-03-30 05:20:29 +0530

    incorporated sidebar of links to website

---
+++ sidebar_menu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
+      <div class="list-group">
+        <a href="home.php" class="list-group-item active">Home</a>
+        <a href="history_hemophilia.php" class="list-group-item">History</a>
+        <a href="glossary_hemophilia.php" class="list-group-item">Glossary</a>
+        <a href="#" class="list-group-item">Medicines</a>
+        <a href="treatment_center_search.php" class="list-group-item">Treatment Centers</a>
+        <a href="guides_hemophilia.php" class="list-group-item">Guides / Manuals</a>
+        <a href="#" class="list-group-item">About us</a>
+        <a href="#" class="list-group-item">Contact us</a>
+      </div>
+    </div>
+</body>
+</html>
