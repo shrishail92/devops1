9077a45e417c5fff5b5940f76a2eef78d475de2f
shrishail92 <shrishail.sh92@gmail.com>
2015-03-30 05:23:00 +0530

    incorporated sidebar of facebook likebox to website

---
+++ fb_like_box.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 	
+	<style type="text/css">
+	#fb-root{
+		background-color: #fff;
+		border:1px solid #ddd;
+		border-radius: 15px;
+	}
+	.pluginBoxDividerGray, .pluginBoxDivider{
+		width: 230px !important;
+	} 
+	.bottomborder {
+	    border-left: medium none;
+	    border-right: medium none;
+	    border-top: medium none;
+	    width: 231px;
+	    border-radius: 15px 15px 0 0;
+	}
+	.uiBoxLight {
+    	background-color: #EDEFF4;
+    	//border: 1px solid #D8DFEA;
+    	padding: 15px 5px 15px 10px;
+    	//font-weight: bold;
+    	font-size: 14px;
+		border-radius: 15px 15px 0 0;
+		
+		color: #666;
+	}
+	</style>
+	<script type="text/javascript">
+		(function(d, s, id) {
+		  	var js, fjs = d.getElementsByTagName(s)[0];
+		  	if (d.getElementById(id)) return;
+		  	js = d.createElement(s); js.id = id;
+		  	js.src = "http://connect.facebook.net/en_US/all.js#xfbml=1&version=v2.0";
+		  	fjs.parentNode.insertBefore(js, fjs);
+		}
+		(document, 'script', 'facebook-jssdk'));
+	</script>
+    <title>FB TEST</title>
+</head>
+
+<body>
+<div id="fb-root">
+<p class="uiBoxLight">Reach us on Facebook</p>
+<div class="fb-like-box" data-href="https://www.facebook.com/RoyaPCtricks" data-width="250" data-height="452" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false"></div>
+</div>
+</body>
+</html>
