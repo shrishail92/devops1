181c171ab4d54058d775c9b49f30897ce02f3951
shrishail92 <shrishail.sh92@gmail.com>
2015-04-01 15:12:30 +0530

    pop up form for contact us in cover page

---
+++ popupform.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Popup contact form</title>
+    <link href="css/popup.css" rel="stylesheet">
+    <script src="js/popup.js"></script>
+</head>
+<!-- Body Starts Here -->
+<body id="body" style="overflow:hidden;">
+<div id="abc">
+    <!-- Popup Div Starts Here -->
+    <div id="popupContact">
+        <!-- Contact Us Form -->
+        <form action="#" id="form" method="post" name="form">
+            <img id="close" src="images/3.png" onclick ="div_hide()">
+            <h2>Contact Us</h2>
+            <hr>
+            <input id="name" name="name" placeholder="Name" type="text">
+            <input id="email" name="email" placeholder="Email" type="text">
+            <textarea id="msg" name="message" placeholder="Message"></textarea>
+            <a href="javascript:%20check_empty()" id="submit">Send</a>
+        </form>
+    </div>
+    <!-- Popup Div Ends Here -->
+</div>
+<!-- Display Popup Button -->
+<h1>Click Button To Popup Form Using Javascript</h1>
+<button id="popup" onclick="div_show()">Popup</button>
+</body>
+<!-- Body Ends Here -->
+</html>
