2dfe16d663f9b27e55abfb9bfa931b044e91a977
shrishail92 <shrishail.sh92@gmail.com>
2015-04-04 12:11:21 +0530

    finished the glossary page

--- glossary_hemophilia.php
+++ glossary_hemophilia.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
+    <meta name="description" content="Glossary of hemophilia">
     <meta name="author" content="shrishail sh">
 
     <title>Fist | Glossary of hemophilia</title>
