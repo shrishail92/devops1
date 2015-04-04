3bdf1ba166713789d04eb912c54ac1ee5ba9de15
shrishail92 <shrishail.sh92@gmail.com>
2015-04-04 12:07:44 +0530

    added home page

--- home.php
+++ home.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Home page">
-    <meta name="author" content="">
+    <meta name="author" content="shrishail sh">
 
     <title>Fist | Home</title>
 
