9444b44471bff3868c37bc7361875fcad017727e
shrishail92 <shrishail.sh92@gmail.com>
2015-04-07 20:15:26 +0530

    included header and footer

--- signin.php
+++ signin.php
@@ -27,133 +27,139 @@
   </head>
 
   <body>
-  	<div class="container marketing" id="container" style="margin-top:40px">
-  		<div class="row">
-  			<div class="col-sm-6 col-md-4 col-md-offset-4">
-          <div>
-          <?php
-              if(isset($_POST['signin']))
-              {
-                //getting post variable
-                $email=strip_tags($_POST['email']);
-                $pass=strip_tags($_POST['password']);
+  <?php include "header.php" ?>
+  <div class="container marketing" id="container" style="margin-top:40px">
+  	 <div class="row">
+  		<div class="col-sm-6 col-md-4 col-md-offset-4">
 
-                $error = array();
-
-                if(empty($email) or !filter_var($email,FILTER_SANITIZE_EMAIL))
+            <?php
+                if(isset($_POST['signin']))
                 {
-                  $error[] = "<div class=\"alert alert-warning\" role=\"alert\">Email id is empty or invalid !</div>";
-                }
-                if(empty($pass))
-                {
-                  $error[] = "<div class=\"alert alert-warning\" role=\"alert\">Please enter password.</div>";
-                }
-                           
-                if(count($error)==0)
-                {
-             
-                        //database configuration
-                        $host = 'localhost';  
-                        $database_name = 'FIST';
-                        $database_user_name = '';  
-                        $database_password = '';  
-             
-                        //if you have database user name & password then connection may be
-                        //$connection=new Mongo("mongodb://$database_user_name:$database_password@$dbhost");
-             
-                        //Currently we are connecting to mongodb without authentication
-                        try
-                        {
-                          $connection = new MongoClient();
-                        }
-                        catch(MongoConnectionException $connectionException)
-                        {
-                          print $connectionException;
-                          exit;
-                        }
-             
-                        //checking the mongo database connection
-                        if($connection)
-                        {
-                          //connecting to database
-                          $databse=$connection->$database_name;
-             
-                          //connect to specific collection
-                          $collection=$databse->user_accounts;
-             
-                          $query=array('email'=>$email);
-                          //checking for existing user
-                          $user=$collection->findOne($query);
+                    //getting post variable
+                    $email=strip_tags($_POST['email']);
+                    $pass=strip_tags($_POST['password']);
 
-                          if(($user['email']==$email) && ($user['password']==md5($pass)))
-                          {
-                            echo "<div class=\"alert alert-success\" role=\"alert\">successful login</div>";
-                            header('Location:home.php');
-                          }
-                          else 
-                          {
-                            echo "<div class=\"alert alert-danger\" role=\"alert\">username or password is wrong !</div>";
-                          }
-                        }
+                    $error = array();
+
+                    if(empty($email) or !filter_var($email,FILTER_SANITIZE_EMAIL))
+                    {
+                      $error[] = '<div class="alert alert-warning" role="alert">Email id is empty or invalid !</div>';
+                    }
+                    if(empty($pass))
+                    {
+                      $error[] = '<div class="alert alert-warning" role="alert">Please enter password.</div>';
+                    }
+
+                    if(count($error)==0)
+                    {
+
+                            //database configuration
+                            $host = 'localhost';
+                            $database_name = 'FIST';
+                            $database_user_name = '';
+                            $database_password = '';
+
+                            //if you have database user name & password then connection may be
+                            //$connection=new Mongo("mongodb://$database_user_name:$database_password@$dbhost");
+
+                            //Currently we are connecting to mongodb without authentication
+                            try
+                            {
+                              $connection = new MongoClient();
+                            }
+                            catch(MongoConnectionException $connectionException)
+                            {
+                              print $connectionException;
+                              exit;
+                            }
+
+                            //checking the mongo database connection
+                            if($connection)
+                            {
+                              //connecting to database
+                              $databse=$connection->$database_name;
+
+                              //connect to specific collection
+                              $collection=$databse->user_accounts;
+
+                              $query=array('email'=>$email);
+                              //checking for existing user
+                              $user=$collection->findOne($query);
+
+                              if(($user['email']==$email) && ($user['password']==md5($pass)))
+                              {
+                                echo '<div class="alert alert-success" role="alert">successful login</div>';
+                                header('Location:home.php');
+                              }
+                              else
+                              {
+                                echo '<div class="alert alert-danger" role="alert">username or password is wrong !</div>';
+                              }
+                            }
+                    }
                 }
-              }
-          ?>
-          <div class="panel panel-default">
-              <div class="panel-heading">
-                  <strong> Sign in to continue</strong>
-              </div>
-              <div class="panel-body">
-                  <form role="form" action="signin.php" method="POST">
-                      <fieldset>
-                          <div class="row">
-                              <div class="center-block">
-                                  <img class="profile-img"
-                                       src="images/signin.png" alt="">
-                              </div>
-                          </div>
-                          <div class="row">
-                              <div class="col-sm-12 col-md-10  col-md-offset-1 ">
-                                  <div class="form-group">
-                                      <div class="input-group">
-                                            <span class="input-group-addon">
-                                                <i class="glyphicon glyphicon-envelope"></i>
-                                            </span>
-                                          <input class="form-control" placeholder="User email" name="email" type="email" required autofocus>
-                                      </div>
-                                  </div>
-                                  <div class="form-group">
-                                      <div class="input-group">
-                                            <span class="input-group-addon">
-                                                <i class="glyphicon glyphicon-lock"></i>
-                                            </span>
-                                          <input class="form-control" placeholder="Password" name="password" type="password" required>
-                                      </div>
-                                  </div>
-                                  <div class="form-group">
-                                      <input type="submit" name="signin" class="btn btn-lg btn-primary btn-block" value="Sign in">
-                                  </div>
-                              </div>
-                          </div>
+            ?>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <strong> Sign in to continue</strong>
+                </div>
+                <div class="panel-body">
+                    <form role="form" action="signin.php" method="POST">
+                        <fieldset>
+                            <div class="row">
+                                <div class="center-block">
+                                    <img class="profile-img"
+                                         src="images/signin.png" alt="">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12 col-md-10  col-md-offset-1 ">
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                              <span class="input-group-addon">
+                                                  <i class="glyphicon glyphicon-envelope"></i>
+                                              </span>
+                                              <input class="form-control" placeholder="User email" name="email" type="email" required autofocus>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                              <span class="input-group-addon">
+                                                  <i class="glyphicon glyphicon-lock"></i>
+                                              </span>
+                                              <input class="form-control" placeholder="Password" name="password" type="password" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="submit" name="signin" class="btn btn-lg btn-primary btn-block" value="Sign in">
+                                    </div>
+                                </div>
+                            </div>
 
-                          <div class="row">
-                              <div class="col-sm-12 col-md-10  col-md-offset-1 ">
-                                <input type="checkbox" value="remember-me">
-                                Remember me
-                              </div>
-                          </div>
+                            <div class="row">
+                                <div class="col-sm-12 col-md-10  col-md-offset-1 ">
+                                  <input type="checkbox" value="remember-me">
+                                  Remember me
+                                </div>
+                            </div>
 
-                      </fieldset>
-                  </form>
-              </div>
-              <div class="panel-footer ">
-                  <a href="#" class="pull-right">Forgot password? </a>
-                  <a href="signup.php" onClick=""> Sign Up Here </a>
-              </div>
-          </div>
-  		</div>
-  	</div> 
-	  <!-- Bootstrap core JavaScript
-    ================================================== -->
+                        </fieldset>
+                    </form>
+                </div>
+                <div class="panel-footer ">
+                    <a href="#" class="pull-right">Forgot password? </a>
+                    <a href="signup.php" onClick=""> Sign Up Here </a>
+                </div>
+            </div>
+
+        </div>
+  	</div>
+  </div>
+
+
+
+            <!-- Bootstrap core JavaScript
+          ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
@@ -161,4 +167,5 @@
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="js/ie10-viewport-bug-workaround.js"></script>
   </body>
+  <?php include "footer.php" ?>
 </html>
