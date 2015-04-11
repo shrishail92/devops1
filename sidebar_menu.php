97d2e3683f843bb3cc85e73818568b41e02095a2
shrishail92 <shrishail.sh92@gmail.com>
2015-04-12 04:38:54 +0530

    finalized to quick navigation links

--- sidebar_menu.php
+++ sidebar_menu.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title>
+        sidebar | links
+    </title>
 </head>
 <body>
     <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
       <div class="list-group">
-        <a href="home.php" class="list-group-item active">Home</a>
-        <a href="history_hemophilia.php" class="list-group-item">History</a>
-        <a href="glossary_hemophilia.php" class="list-group-item">Glossary</a>
-        <a href="#" class="list-group-item">Medicines</a>
-        <a href="treatment_center_search.php" class="list-group-item">Treatment Centers</a>
+        <span class="list-group-item list-group-item-heading">Quick Navigation Links</span>
         <a href="guides_hemophilia.php" class="list-group-item">Guides / Manuals</a>
-        <a href="#" class="list-group-item">About us</a>
-        <a href="#" class="list-group-item">Contact us</a>
+        <a href="about_hemophilia.php" class="list-group-item">About Hemophilia</a>
+        <a href="treatment_center_search.php" class="list-group-item">Treatment Centers</a>
+        <a href="statistics_hemophilia.php" class="list-group-item">Stats of Hemophilia</a>
+        <a href="videos_hemophilia.php" class="list-group-item">Videos - Hemophilia</a>
+        <a href="glossary_hemophilia.php" class="list-group-item">Glossary for this site</a>
+        <a href="history_hemophilia.php" class="list-group-item">History of Hemophilia</a>
       </div>
     </div>
 </body>
