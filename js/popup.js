da710c4203b7e145078e5cb707f9c64b92367e06
shrishail92 <shrishail.sh92@gmail.com>
2015-04-12 20:46:43 +0530

    added progressbar

--- js/popup.js
+++ js/popup.js
@@ -6,12 +6,14 @@ function progress()
 {
     if (document.getElementById('name').value != "" && document.getElementById('email').value != "" && document.getElementById('msg').value != "")
     {
-        var id='progress';
-        var myClassName=" progress"; //must keep a space before class name
+        var id = 'progress';
+        var myClassName = " progress"; //must keep a space before class name
         var d;
-        d=document.getElementById(id);
-        d.className=d.className.replace(myClassName,""); // first remove the class name if that already exists
-        d.className = d.className + myClassName; // adding new class name
+        d = document.getElementById(id);
+        d.className = d.className.replace(myClassName, ""); // first remove the class name if that already exists
+        d.className = d.className + myClassName;
+        // adding new class name
+        document.getElementById('submitbutton').value="sending";
     }
 }
 
