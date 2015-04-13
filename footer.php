8c6335047f010936de67978e50dd902ef903a3e5
shrishail92 <shrishail.sh92@gmail.com>
2015-04-13 23:49:33 +0530

    formatted the file

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,33 +13,36 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <!--[if lt IE 9]>
+    <script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
     <script src="js/ie-emulation-modes-warning.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-      <script src="js/html5shiv.min.js"></script>
-      <script src="js/respond.min.js"></script>
+    <script src="js/html5shiv.min.js"></script>
+    <script src="js/respond.min.js"></script>
     <![endif]-->
 
     <!-- Custom styles for this template -->
     <link href="carousel.css" rel="stylesheet">
 
-  </head>
+</head>
 <!-- NAVBAR
 ================================================== -->
-  <body>
-	  <!-- FOOTER -->
-      <footer>
-        <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>&copy; 2015 Future India Social Trust - FIST, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-      </footer>
-      <!-- box-shadow: none|h-shadow v-shadow blur spread color |inset|initial|inherit;-->
-	  <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/docs.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
-    <a href="#" class="scrollToTop" title="Scroll to top"><i class="glyphicon glyphicon-chevron-up"></i></a>
-  </body>
+<body>
+<!-- FOOTER -->
+<footer>
+    <p class="pull-right"><a href="#">Back to top</a></p>
+
+    <p>&copy; 2015 Future India Social Trust - FIST, Inc. &middot; <a href="#">Privacy</a> &middot; <a
+            href="#">Terms</a></p>
+</footer>
+<!-- box-shadow: none|h-shadow v-shadow blur spread color |inset|initial|inherit;-->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/docs.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="js/ie10-viewport-bug-workaround.js"></script>
+<a href="#" class="scrollToTop" title="Scroll to top"><i class="glyphicon glyphicon-chevron-up"></i></a>
+</body>
 </html>
